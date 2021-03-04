b056418cabf1c23925f4b73deb1e50076a4dbafc
siphera <salmansiphenkosi@gmail.com>
2021-03-04 14:03:10 +0200

    fetching data from the api

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,5 +22,56 @@ function getPosts() {
   }
   
   getPosts();
+
+  // get the bustket items
+  function receipt() {
+    // Get element to change
+    let list = document.getElementById("receipt");
   
+    // Fetch the data
+    fetch("http://127.0.0.1:5000/busk")
+      .then((response) => response.json())
+      .then((json) => {
+        console.log(json);
+        json.forEach((blog) => receiptItems(blog));
+      });
+  }
   
+  function receiptItems(blog) {
+    const item = `
+                    <td>${blog.pid}</td>
+                    <td>${blog.product}</td>
+                    <td>R${blog.price}</td>
+                    <td>${blog.quantity}</td>
+                `;
+    let list = document.getElementById("receipt");
+    console.log("Hello");
+    list.innerHTML += item;
+  }
+  
+  receipt();
+  
+    // get the bustket items
+    function total() {
+      // Get element to change
+      let list = document.getElementById("order-total");
+    
+      // Fetch the data
+      fetch("http://127.0.0.1:5000/total")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          json.forEach((blog) => totaldisp(blog));
+        });
+    }
+    
+    function totaldisp(blog) {
+      const item = `
+                      <span>R${blog[0]}</span>
+                  `;
+      let list = document.getElementById("order-total");
+      console.log("Hello");
+      list.innerHTML += item;
+    }
+    
+    total();

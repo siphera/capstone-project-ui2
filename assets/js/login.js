08e7d88144ae47999eb9cf79a12edb26f6be55b1
siphera <salmansiphenkosi@gmail.com>
2021-03-09 16:05:20 +0200

    mod

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,10 +1,24 @@
-function login(){
-    let login_form = document.getElementById("login")
-    let login_inputs = login_form.getElementsByTagName("input")
-
-    let user = login_inputs[0].value;
-    let password = login_inputs[1].value;
-
+function login() {
+    let loginForm = document.getElementById("login");
+    let inputs = loginForm.getElementsByTagName("input");
+  
+    let user = inputs[0].value;
+    let password = inputs[1].value;
+  
     let users;
-    fetch("http://127.0.0.1:5000/")
-}
+    fetch("http://127.0.0.1:5000/login")
+    .then((response) => response.json())
+    .then((json) =>{
+      console.log(json);
+      users = json;
+      console.log(user, password, users);
+  
+      let logged = users.filter((userProfile) => {
+        return userProfile.user == user && userProfile.password == password;
+      });
+      console.log(logged);
+      if (logged.length >= 1){
+        window.location.href = `./index.html`;
+      }
+    })
+  }
--- assets/js/register.js
+++ assets/js/register.js
@@ -2,17 +2,17 @@ function register() {
     let form = document.getElementById("register");
     const inputs = form.getElementsByTagName("input");
 
-    fetch("http://127.0.0.1:5000/login", {
+    fetch("http://127.0.0.1:5000/register/", {
         
             method: "POST",
             body: JSON.stringify({
-                user: inputs[0].value,
+                username: inputs[0].value,
                 role: inputs[1].value,
                 password: inputs[2].value,
             }),
             headers: {
                 "Content-type": "application/json; charset=UTF-8",
-            },
+            }
         })
         .then((response) => response.json())
         .then((json) => {

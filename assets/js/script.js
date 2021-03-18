3669d76299bab1594ffd3d7a7001144fc15a11be
siphera <salmansiphenkosi@gmail.com>
2021-03-18 11:51:51 +0200

    .

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,7 @@ function getPosts() {
   }
   
   function createBlogItem(blog) {
-    const item = `<button>
+    const item = `<button onclick="buy(${blog.pid})">
                     <span class="item">R${blog.price}</span></br>
                     <span class="category">${blog.product}</span>
                   </button>`;
@@ -22,53 +22,97 @@ function getPosts() {
   }
   getPosts(); // displaying the items on the buttons
 
-  // get the bustket items
-  function receipt() {
-    // Get element to change
-    let list = document.getElementById("receipt");
-  
-    // Fetch the data
-    fetch("https://cryptic-plains-12434.herokuapp.com/bask")
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        json.forEach((blog) => receiptItems(blog));
-      });
-  }
-  
-  function receiptItems(blog) {
-    const item = `
-                    <td>${blog.pid}</td>
-                    <td>${blog.product}</td>
-                    <td>R${blog.price}</td>
-                    <td><button style="background-color: red; color: white;">Delete</button></td>
-                `;
-    let list = document.getElementById("receipt");
-    console.log("Hello");
-    list.innerHTML += item;
-  }
-  
-  receipt();
+  // ==============================================================
+  let cart =[]
 
-    // get the basket items
-    function total() {
-      // Get element to change
-      let list = document.getElementById("order-total");
+  fetch('https://cryptic-plains-12434.herokuapp.com/items')
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    cart = data;
+  })
+
+  function buy(pid) {
+    let shownItemList = document.getElementById('receip-list');
+    let filteredProducts = cart.filter(foodcart => {
+      return foodcart.pid == pid;
+    })
+    filteredProducts.forEach(item => {
+      let total = parseInt(document.getElementById('order-total').innerHTML);
+      let product_price = parseInt(item.price)
+      total = total + product_price;
+      shownItemList.innerHTML += `
+      <p class="cart" style="color:white">${item.product} ${item.price}</p>`
+      document.getElementById('order-total').innerHTML = total;
+    });
+    console.log(filteredProducts)
+  }
+
+  function clear() {
+    let total = document.getElementsByClassName("order-total")[0].innerHTML;
+    alert(`Thank you for your purchase. Next customer please`);
+    document.getElementsByClassName("order-total")[0].innerHTML = "0";
+    let clear = "";
+    let rec = document.getElementById("receip-list");
+    rec.innerHTML = clear;
+  }
+
+  
+  // function receiptItems(blog) {
+  //   const item = `
+  //                   <td>${blog.pid}</td>
+  //                   <td>${blog.product}</td>
+  //                   <td>R${blog.price}</td>
+  //                   <td><button style="background-color: red; color: white;">Delete</button></td>
+  //               `;
+  //   let list = document.getElementBy  //   let inCartItems = products.filter((product) => blog.pid == pid);
+  //   let selectedItem = inCartItems[0];
+  //   let item = `
+  //       <td>${blog.pid}</td>
+  //       <td>${blog.product}</td>
+  //       <td>R${blog.price}</td>
+  //       <td><button style="background-color: red; color: white;">Delete</button></td>
+  //       <li class="rec-li" id="product${pid}" product-price=${selectedItem.price}>
+  //         ${selectedItem.product_name} : ${selectedItem.price}
+  //       <button onclick="removefromCart(${pid})">Remove</button></li>
+  //     `;
+  //   cart.innerHTML += item;
+  //   console.log(products);
+  
+  //   function calcTotal() {
+  //     let recieptTotal = document.getElementsByClassName("order-total")[0];
+  //     let y = parseInt(recieptTotal.innerHTML);
+  //     let x = document
+  //       .getElementById("product" + pid)
+  //       .getAttribute("product-price");
+  
+  //     let total = parseInt(y) + parseInt(x);
+ Id("receipt");
+  //   console.log("Hello");
+  //   list.innerHTML += item;
+  // }
+  
+  // receipt();
+
+    // // get the basket items
+    // function total() {
+    //   // Get element to change
+    //   let list = document.getElementById("order-total");
     
-      // Fetch the data
-      fetch("https://cryptic-plains-12434.herokuapp.com/total")
-        .then((response) => response.json())
-        .then((json) => {
-          console.log(json);
-          json.forEach((blog) => totaldisp(blog));
-        });
-    }
-    function totaldisp(blog) {
-      const item = `
-                      <span>R${blog[0]}</span>
-                  `;
-      let list = document.getElementById("order-total");
-      console.log("Hello");
-      list.innerHTML += item;
-    }
-    total();
+    //   // Fetch the data
+    //   fetch("https://cryptic-plains-12434.herokuapp.com/total")
+    //     .then((response) => response.json())
+    //     .then((json) => {
+    //       console.log(json);
+    //       json.forEach((blog) => totaldisp(blog));
+    //     });
+    // }
+    // function totaldisp(blog) {
+    //   const item = `
+    //                   <span>R${blog[0]}</span>
+    //               `;
+    //   let list = document.getElementById("order-total");
+    //   console.log("Hello");
+    //   list.innerHTML += item;
+    // }
+    // total();
